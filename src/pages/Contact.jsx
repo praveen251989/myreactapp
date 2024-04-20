aa4583e6d51e1378d72bbd11fd82622124e1638e
Praveen251989 <praveen251989@gmail.com>
2024-04-20 20:35:21 +0530

    Contact page change

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,41 +11,46 @@ import { doc, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 const Contact = (props) => {
-    const [value, setValue] = useState('yes');
-    const spacing = 8;
-    const {name} = props;
-    const [contactInfo, setContactInfo] = useState({});
-    useEffect(() => {
-        getContact();
-    }, []);
-    const getContact = async() => {
-        const docRef = doc(db, "admin-users", name);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            setContactInfo(docSnap.data());
-            //console.log("Document data:", contactInfo.title);
-        } else {
-            console.log("No such document!");
-        }
-    }
+	const spacing = 8;
+	const { name } = props;
+	const [contactInfo, setContactInfo] = useState({});
+	useEffect(() => {
+		getContact();
+	}, []);
+	const getContact = async () => {
+		const docRef = doc(db, "admin-users", name);
+		const docSnap = await getDoc(docRef);
+		if (docSnap.exists()) {
+			setContactInfo(docSnap.data());
+		} else {
+			alert("No such document!");
+		}
+	};
 	return (
 		<div>
 			<TextField
 				name="title"
 				label="Title of Business"
-                InputProps={{readOnly: true}}
-                defaultValue={contactInfo.title}
+				InputProps={{ readOnly: true }}
+				defaultValue=" "
+				value={contactInfo.title}
 			/>
 			<Stack direction="row" useFlexGap spacing={spacing}>
 				<TextField
 					name="firstName"
 					label="Owner FN"
 					margin="normal"
+					defaultValue=" "
+					value={contactInfo.firstName}
+					InputProps={{ readOnly: true }}
 				/>
 				<TextField
 					name="lastName"
 					label="Owner LN"
 					margin="normal"
+					defaultValue=" "
+					value={contactInfo.lastName}
+					InputProps={{ readOnly: true }}
 				/>
 			</Stack>
 			<Stack direction="row" useFlexGap spacing={spacing}>
@@ -53,40 +58,61 @@ const Contact = (props) => {
 					name="email"
 					label="Email"
 					margin="normal"
+					defaultValue=" "
+					value={contactInfo.email}
+					InputProps={{ readOnly: true }}
 				/>
 				<TextField
 					name="phone"
 					label="Phone"
 					margin="normal"
+					defaultValue=" "
+					value={contactInfo.phone}
+					InputProps={{ readOnly: true }}
 				/>
 			</Stack>
-            <Stack direction="row" useFlexGap spacing={spacing}>
-                <TextField
-                    name="address1"
-                    label="Address 1"
-                    margin="normal"
-                />
-                <TextField
-                    name="address2"
-                    label="Address 2"
-                    margin="normal"
-                />
-            </Stack>
+			<Stack direction="row" useFlexGap spacing={spacing}>
+				<TextField
+					name="address1"
+					label="Address 1"
+					margin="normal"
+					defaultValue=" "
+					value={contactInfo.address1}
+					InputProps={{ readOnly: true }}
+				/>
+				<TextField
+					name="address2"
+					label="Address 2"
+					margin="normal"
+					defaultValue=" "
+					value={contactInfo.address2}
+					InputProps={{ readOnly: true }}
+				/>
+			</Stack>
 			<TextField
 				name="license"
 				label="License"
 				margin="normal"
+				defaultValue=" "
+				value={contactInfo.license}
+				InputProps={{ readOnly: true }}
 			/>
 			<Stack direction="row" useFlexGap spacing={spacing}>
 				<TextField
 					name="gst"
 					label="GST"
 					margin="normal"
+					defaultValue=" "
+					value={contactInfo.gst}
+					InputProps={{ readOnly: true }}
 				/>
 				<TextField
 					name="cst"
 					label="CST"
 					margin="normal"
+					defaultValue=" "
+					value={contactInfo.cst}
+					InputProps={{ readOnly: true }}
 				/>
 			</Stack>
 			<Stack direction="row" useFlexGap spacing={2}>
@@ -103,33 +129,40 @@ const Contact = (props) => {
 				</Typography>
 				<RadioGroup
 					name="experience"
-					value={value}
+					value={contactInfo.experience}
 					row
+					InputProps={{ readOnly: true }}
 				>
 					<FormControlLabel
 						value="yes"
-						control={<Radio />}
+						control={<Radio InputProps={{ readOnly: true }} />}
 						label="Yes"
 					/>
 					<FormControlLabel
 						value="no"
-						control={<Radio />}
+						control={<Radio InputProps={{ readOnly: true }} />}
 						label="No"
 					/>
 				</RadioGroup>
 			</Stack>
-            <Stack direction="row" useFlexGap spacing={spacing}>
-            <TextField
-                name="description"
-                margin="normal"
-                label="Description"
-            />
-			<TextField
-				name="bankActDetails"
-				label="Bank Account Details"
-				margin="normal"
-			/>
-            </Stack>
+			<Stack direction="row" useFlexGap spacing={spacing}>
+				<TextField
+					name="description"
+					margin="normal"
+					label="Description"
+					defaultValue=" "
+					value={contactInfo.description}
+					InputProps={{ readOnly: true }}
+				/>
+				<TextField
+					name="bankActDetails"
+					label="Bank Account Details"
+					margin="normal"
+					defaultValue=" "
+					value={contactInfo.bankActDetails}
+					InputProps={{ readOnly: true }}
+				/>
+			</Stack>
 		</div>
 	);
 };
